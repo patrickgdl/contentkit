a4ee9ea3adf4177cadf44e3c92fb6f121dd70ef8
Patrick Lima <patrick.lima@picpay.com>
2022-02-02 11:21:05 -0300

    chore: base tailwind + remotion

---
+++ next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const withPWA = require('next-pwa');
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+  },
+  reactStrictMode: true,
+  experimental: {
+    modern: true,
+  },
+  images: {
+    domains: ['images.unsplash.com'],
+  },
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+});
---
+++ pages/_document.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="shortcut icon" href="/favicon.png" />
+        </Head>
+        <body>
+          <Main />
+          <div id="shadowbox" />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
